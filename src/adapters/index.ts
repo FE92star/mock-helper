622a81c8c9fabc6a6606a8007dcba8d3e8b14b15
FE92star <1737765939@qq.com>
2023-10-18 19:52:42 +0800

    feat: update type

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,4 +1,4 @@
-export function apiAdapters(data: { res_body: any; [key: string]: any }) {
+export function apiAdapters(data: { res_body: any & Record<string, any> }) {
   return {
     ...data,
     response: JSON.parse(data.res_body) || {},
